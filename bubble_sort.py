1a4f1844b0e64abdd25c41e6ef75c593cc2c440f
Mark H <englishknight1301@gmail.com>
2023-08-15 15:16:21 +0000

    Bubble Sort - Python

---
+++ bubble_sort.py
@@ -0,0 +1,22 @@
+import random
+unsorted_array = [0]*100000
+
+size = len(unsorted_array)
+
+for x in range (0,size):
+    unsorted_array [x] = random.randint(0,100000)
+    print (unsorted_array [x])
+
+notsort = 1
+
+while(notsort == 1):
+    notsort = 0
+    for i in range (0,size-1):
+        if (unsorted_array[i] > unsorted_array[i+1]):
+            temp = unsorted_array[i]
+            unsorted_array[i] = unsorted_array[i+1]
+            unsorted_array[i+1] = temp
+            notsort = 1
+
+for x in range (0,size):
+    print (unsorted_array [x])
